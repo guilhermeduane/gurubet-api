16df091ae455ff47753554505bf05da13a5684f0
guilhermeduane <gui.duane@gmail.com>
2024-12-17 21:37:23 +0100

    enable cors in the main rest method. Just for test purposes, later i will implement a config to enable cors in all rest controllers.

--- src/main/java/com/gurubet/controller/FixtureController.java
+++ src/main/java/com/gurubet/controller/FixtureController.java
@@ -2,6 +2,7 @@ package com.gurubet.controller;
 
 import com.gurubet.domain.Fixture;
 import com.gurubet.service.FixtureService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class FixtureController {
         this.fixtureService = fixtureService;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping
     public List<Fixture> getAllFixtures() {
         return fixtureService.getAllFixtures();
